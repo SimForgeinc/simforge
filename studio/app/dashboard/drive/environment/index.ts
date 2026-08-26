0cc138b2f0d778ecde8720584ce1dc5fa26ec9e2
Michael <michael@simforge.ai>
2026-08-26 13:02:32 -0700

    Remove Drive site-time lighting override

--- studio/app/dashboard/drive/environment/index.ts
+++
@@ -1,8 +0,0 @@
-export { useSiteTimeOfDay, type SiteTimeOfDayState } from "./use-site-time-of-day";
-export {
-  lightingForSolarPosition,
-  solarPosition,
-  solarPositionMoved,
-  type SolarLighting,
-  type SolarPosition,
-} from "./solar-position";
